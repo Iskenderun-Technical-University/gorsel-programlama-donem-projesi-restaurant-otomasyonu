51bdbe4bcccc66db0e5b494578fbc9bc77c5a2a5
ademtugar <ademtulgar.mdbf21@iste.edu.tr>
2022-11-09 22:36:25 +0300

    kullanıcı arayüzü oluşturuldu

--- RestoranOtomasyonu/RestoranOtomasyonu/Form1.cs
+++ RestoranOtomasyonu/RestoranOtomasyonu/Form1.cs
@@ -17,10 +17,11 @@ namespace RestoranOtomasyonu
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            label1.Text = "deneme baþarýlý";
-            label2.Text = "problemler çözüldü";
+            
         }
     }
 }
