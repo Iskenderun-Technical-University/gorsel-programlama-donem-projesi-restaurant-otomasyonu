22c0c66e64110791f633921aea3ecf3d4bbe7795
edanur <eda.celik.mdbf21@iste.edu.tr>
2022-11-09 22:08:49 +0300

    buton eklendi

--- RestoranOtomasyonu/RestoranOtomasyonu/Form1.cs
+++ RestoranOtomasyonu/RestoranOtomasyonu/Form1.cs
@@ -16,5 +16,10 @@ namespace RestoranOtomasyonu
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "deli özlem";       
+        }
     }
 }
